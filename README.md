fd7ca8ab6fa69bf4b648c4aa52f4e3eb36628729
Fenglin <3181693974@qq.com>
2024-12-09 21:28:11 +0800

    晚安插件V2.0.0

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 <div align="center">
 
-# [Koishi](https://koishi.chat) 插件: 晚安签到插件 
+# [Koishi](https://koishi.chat) 插件: 晚安签到插件
 
 [![npm](https://img.shields.io/npm/v/koishi-plugin-sleepsign-in?style=flat-square)](https://www.npmjs.com/package/koishi-plugin-sleepsign-in)
 
@@ -12,7 +12,7 @@
 
 ### 使用 **koishi WebUI** 或者 **Koishi桌面程序** 安装
 
-在插件市场搜索 `koishi-plugin-sleepsign-in` 并安装即可。
+在插件市场搜索 `sleepsign-in` 并安装即可。
 
 ### 使用命令行安装
 
@@ -23,20 +23,41 @@ yarn add koishi-plugin-sleepsign-in
 # 或者使用 npm
 npm i koishi-plugin-sleepsign-in
 ```
+
 ## 插件使用
 
 安装成功之后，和你的机器人对话即可。
 
 ### 触发关键词
+
 - `晚安` - 和他说晚安告诉他你要睡觉了 他会给你一个晚安排行
 
 > 注意：你可以在配置文件中修改触发关键词。
 
 ### 可用命令
+
 `查询晚安次数` - 查看你的晚安次数
 
 ## 许可
 
 该项目根据 [MIT](./LICENSE) 许可证获得许可。
 
+## 更新日志
+
+#### V1.0.0 实现基础功能
+
+* 晚安签到
+* 签到排名
+* 晚安次数统计
+
+#### V2.0.0 添加配置项
+
+* 添加自定义配置项
+
+1. 自定义签到时间
+2. 自定义签到触发词和其他提示消息
+
+* 修复更新数据库失败的问题
+
 </div>
+
--- src/index.ts
+++ src/index.ts
@@ -3,9 +3,36 @@ import { Context, h, Schema } from 'koishi'
 export const name = 'sleepsign-in'
 export const inject = ['database']
 
-export interface Config {}
+export interface IConfig {
+  enableSignTime: boolean
+  signStartTime: number
+  signEndTime: number
+  goodNightMsg: string
+  missedSignMsg: string
+  repeatSignMsg: string
+  succeedMsg: string
+}
 
-export const Config: Schema<Config> = Schema.object({})
+export const Config: Schema<IConfig> = Schema.intersect([
+  Schema.object({
+    enableSignTime: Schema.boolean().default(false).description('是否自定义签到时间，默认签到时间为晚上22点到明早6点'),
+  }),
+  Schema.union([
+    Schema.object({
+      enableSignTime: Schema.const(true).required(),
+      signStartTime: Schema.number().default(22).description('签到开始时间'),
+      signEndTime: Schema.number().default(6).description('签到结束时间')
+    }),
+    Schema.object({}),
+  ]).description('签到时间配置') as Schema,
+
+  Schema.object({
+    goodNightMsg: Schema.string().default('晚安').description('晚安签到的触发消息'),
+    missedSignMsg: Schema.string().default('现在不是睡觉时间哦！').description('未到晚安时间的提醒消息'),
+    repeatSignMsg: Schema.string().default('你已经晚安过了哦！').description('重复晚安的提醒消息'),
+    succeedMsg: Schema.string().default('晚安！你是第-rank个晚安的群友哦！').description('晚安成功的提醒消息，-rank为排名，-time为晚安时间')
+  }).description('晚安消息配置')
+])
 
 declare module 'koishi' {
   interface Tables {
@@ -20,7 +47,8 @@ export interface User_sign_in {
   sign_time: string
 }
 
-export function apply(ctx: Context) {
+export function apply(ctx: Context, config: IConfig) {
+  const logger = ctx.logger(ctx.name)
   // 判断是否存在数据表
   if (!ctx.database.tables.user_sign_in) {
     ctx.database.extend('user_sign_in', {
@@ -37,55 +65,73 @@ export function apply(ctx: Context) {
     })
   }
 
-  // 晚安命令
-  ctx.command('晚安').action(async ({ session }) => {
-    let hour = new Date().getHours()
-    // 判断是否在签到时间内
-    if (hour < 6 || hour >= 21) {
-      await session.send("现在不是睡觉时间哦！")
-      return;
-    }
-
-    try {
-      let date = new Date().toLocaleDateString()
-      const rows = await ctx.database.get('user_sign_in', {
-        user_id: session.userId
-      })
-      // 判断是否存在该用户
-      if (!(rows.length > 0)) {
-        // 不存在则创建
-        await ctx.database.create('user_sign_in', {
-          user_id: session.userId,
-          count: 1,
-          sign_time: date
-        })
-      } else {
-        // 签到时间是否是今天
-        if (rows[0].sign_time === date) {
-          await session.send(h('at', { id: session.userId }) + "你已经晚安过了！")
-          return;
+  // 监听消息事件 如果是晚安则进行签到
+  ctx.middleware(async (session, next) => {
+    // 判断消息内容是否是晚安
+    if (session.content === config.goodNightMsg) {
+      let date = new Date()
+      // logger.info('当前时间：' + hour)
+      // 判断是否在签到时间内
+      if (config.enableSignTime) {
+        let hour = date.getHours()
+        if (config.signEndTime - config.signStartTime > 0) {
+          if (!(hour >= config.signStartTime && hour <= config.signEndTime)) {
+            return (h('at', { id: session.userId }) + config.missedSignMsg)
+          }
+        } else if (config.signEndTime - config.signStartTime < 0) {
+          if (hour < config.signStartTime || hour >= config.signEndTime) {
+            return (h('at', { id: session.userId }) + config.missedSignMsg)
+          }
         }
-        // 存在则更新
-        await ctx.database.set('user_sign_in', {
-          count: rows[0].count + 1,
-          sign_time: date
-        }, {
+      } else {
+        let hour = date.getHours()
+        if (hour < 6 || hour >= 22) {
+          return (h('at', { id:session.userId }) + config.missedSignMsg)
+        }
+      }
+
+      try {
+        let dateStr = date.toLocaleDateString()
+        const rows = await ctx.database.get('user_sign_in', {
           user_id: session.userId
         })
+        // 判断是否存在该用户
+        if (!(rows.length > 0)) {
+          // 不存在则创建
+          await ctx.database.create('user_sign_in', {
+            user_id: session.userId,
+            count: 1,
+            sign_time: dateStr
+          })
+        } else {
+          // 签到时间是否是今天
+          if (rows[0].sign_time === dateStr) {
+            return (h('at', { id: session.userId }) + config.repeatSignMsg)
+          }
+          // 存在则更新
+          await ctx.database.set('user_sign_in', {
+            user_id: session.userId
+          }, {
+            count: rows[0].count + 1,
+            sign_time: dateStr
+          })
+        }
+
+        const succeedMsg = config.succeedMsg
+        .replace('-rank', (await ctx.database.get('user_sign_in', {sign_time: dateStr})).length.toString())
+        .replace('-time', date.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' }))
+
+        return succeedMsg
+      }catch(err){
+        logger.error('晚安失败，原因：' + err)
+        return (h('at', { id: session.userId }) + '晚安失败，请稍后再试')
       }
-      // 发送晚安消息
-      const nowDateRows = await ctx.database.get('user_sign_in', {
-        sign_time: date
-      })
-      await session.send(h('at', { id: session.userId }) + "晚安！你是第" + nowDateRows.length + "个晚安的群友哦！")
-    }catch(err){
-      console.log('签到失败，原因：' + err)
-      ctx.logger.error('晚安失败，原因：' + err)
-      await session.send(h('at', { id: session.userId }) + '晚安失败，请稍后再试')
+    } else {
+      return next()
     }
   })
 
-  // 查询晚安次数
+  // 查询晚安次数命令
   ctx.command('查询晚安次数').action(async ({ session }) => {
     try {
       const rows = await ctx.database.get('user_sign_in', {
@@ -97,8 +143,7 @@ export function apply(ctx: Context) {
         await session.send(h('at', { id: session.userId }) + "你还没有晚安过呢")
       }
     }catch(err){
-      console.log('查询失败，原因：' + err)
-      ctx.logger.error('查询失败，原因：' + err)
+      logger.error('查询失败，原因：' + err)
       await session.send(h('at', { id: session.userId }) + '查询失败，请稍后再试')
     }
   })
